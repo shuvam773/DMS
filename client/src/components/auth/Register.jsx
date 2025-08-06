2de281a0bfbc865c2c7b6d19c28cb11eb7424093
shuvam773 <shuvam.sk25@gmail.com>
2025-08-06 21:48:57 +0530

    lil tweaking

--- client/src/components/auth/Register.jsx
+++ client/src/components/auth/Register.jsx
@@ -91,7 +91,7 @@ const Register = () => {
     <div className="min-h-screen bg-gray-50 flex flex-col justify-center py-12 sm:px-6 lg:px-8">
       <div className="mt-6 sm:mx-auto sm:w-full sm:max-w-md">
         <div className="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
-          <div className="sm:mx-auto sm:w-full sm:max-w-md">
+          <div className=" pb-6 sm:mx-auto sm:w-full sm:max-w-md">
             <div>
               <img
                 src={backgroundImage}
